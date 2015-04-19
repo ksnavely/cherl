7b990bce1a5915475cfb8e5a058039574a8e6bd3
Kyle Snavely <ksnavely@cloudant.com>
2015-04-19 17:31:20 -0400

    I be chattin

--- src/cherl_client.erl
+++ src/cherl_client.erl
@@ -1,6 +1,6 @@
 -module(cherl_client).
 
--export([create/3, chat/3, loop/2, start/1]).
+-export([create/3, chat/2, loop/1, start/1]).
 -export([handle_cast/2, init/1, terminate/2]).
 
 -behavior(gen_server).
@@ -11,9 +11,9 @@ create(Node, Username, Password) ->
   %% create
   %% Register a username/password with the cherl server. Start a client OTP
   %% application which can do chats. This one does it to a remote cherl_server.
-  spawn(Node, gen_server, call, [cherl_server, {create_client, Username, Password}]),
+  spawn(Node, gen_server, call, [cherl_server, {create_client, Username, Password, node()}]),
   {ok, Pid} = gen_server:start(?MODULE, {Username, Password}, []),
-  register(list_to_atom(Username), Pid).
+  register(client, Pid).
 
 start([Node, UsernameAtom]) ->
   Username = atom_to_list(UsernameAtom),
@@ -24,21 +24,25 @@ start([Node, UsernameAtom]) ->
   create(Node, Username, Pwd),
   % Now we need a loop to prompt for chats, later recieve chat messages
   io:format("Chat session started.~n", []),
-  loop(Node, list_to_atom(Username)).
+  loop(Node).
 
-loop(Node, Client) ->
+loop(Node) ->
   Msg = re:replace(io:get_line("=> "), "[\\t\\n]+", "", [global,{return,list}]),
-  chat(Node, Client, Msg),
-  loop(Node, Client).
+  chat(Node, Msg),
+  loop(Node).
 
-chat(Node, Client, Message) ->
-  gen_server:cast(Client, {chat, Node, Message}).
+chat(Node, Message) ->
+  gen_server:cast(client, {chat, Node, Message}).
 
 %% Client OTP callbacks below
 
 handle_cast({chat, Node, Message}, {Username, Password}) ->
   %% Fancy remote chat
   spawn(Node, gen_server, cast, [cherl_server, {chat, Username, Password, Message}]),
+  {noreply, {Username, Password}};
+handle_cast({server_message, Sender, Message}, {Username, Password}) ->
+  %% The client has received a chat from the server
+  io:format("~s: ~s~n", [Sender, Message]),
   {noreply, {Username, Password}}.
 
 init({Username, Password}) ->
--- src/cherl_server.erl
+++ src/cherl_server.erl
@@ -1,6 +1,6 @@
 -module(cherl_server).
 
--export([server/0, test/1]).
+-export([server/0]).
 -export([handle_call/3, handle_cast/2, init/1, terminate/2]).
 
 -behavior(gen_server).
@@ -14,23 +14,38 @@ server() ->
   {ok, Pid} = gen_server:start(?MODULE, [], []),
   register(cherl_server, Pid).
 
-test(Message) ->
-  gen_server:cast(?MODULE, {cherl_server, Message}).
+%% Non-api functions
+
+chat(Username, Message) ->
+  % We've recieved an authenticated chat
+  io:format("~s: ~s~n", [Username, Message]),
+  ets:foldl(
+    fun(Entry, Acc) ->
+      {IterUsername, {_, IterNode}} = Entry,
+      case Username of
+        IterUsername -> Acc; % Don't echo the user's message to themselves
+        _ -> spawn(IterNode, gen_server, cast, [client, {server_message, Username, Message}]),
+             Acc % Just recycle this
+      end
+    end,
+    0,
+    server_users
+  ).
 
 %% Server OTP callbacks below
 
-handle_call({create_client, Username, Password}, _From, LoopData) ->
+handle_call({create_client, Username, Password, Node}, _From, LoopData) ->
   % Register the username/password with our server_users ETS table
   case ets:lookup(server_users, Username) of
    [] -> io:format("[INFO] Did not find ~s, inserting.~n", [Username]),
-         ets:insert(server_users, {Username, Password});
+         ets:insert(server_users, {Username, {Password, Node}});
     _ -> io:format("[WARNING] User ~s already exists!~n", [Username])
   end,
   {reply, ok, LoopData}.
 
 handle_cast({chat, Username, Password, Message}, LoopData) ->
   case ets:lookup(server_users, Username) of
-    [{Username, Password}] -> io:format("~s: ~s!~n", [Username, Message]);
+    [{Username, {Password, _}}] -> chat(Username, Message);
     [] -> io:format("[WARNING] No such user: ~s~n", [Username]);
     _ -> io:format("[WARNING] Unauthorized access attempt for: ~s~n", [Username])
   end,
